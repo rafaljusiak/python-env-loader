81eac1a5a3aa6c06edd2a635fda470032652bb06
rafaljusiak <rafal.jusiak@jmr.pl>
2020-01-27 14:11:51 +0100

    added base tests

---
+++ tests/conftest.py
@@ -0,0 +1,24 @@
+import pytest
+
+
+@pytest.fixture
+def env_file(tmp_path):
+    file = tmp_path / '.env'
+    file.write_bytes(b"""
+A=default
+BOOL=true
+INT=100
+FLOAT=1.25
+INT_LIST=5,1,2
+IP_LIST=127.0.0.1,0.0.0.0,localhost
+STRING_LIST=cat,dog,mouse,pig
+MIXED_LIST=1,cat,2.5
+    """)
+    yield str(file)
+
+
+@pytest.fixture
+def env_local_file(tmp_path):
+    file = tmp_path / '.env.local'
+    file.write_bytes(b'A=local')
+    yield str(file)
---
+++ tests/test_default_env.py
@@ -0,0 +1,62 @@
+from src.python_env_loader import load_env, EnvTypes
+
+
+def test_local_is_overriding_base_env(tmp_path, env_file, env_local_file):
+    env = load_env(env_type=EnvTypes.DEFAULT_ENV, dir_path=tmp_path, raise_file_not_found=True)
+    v = env.get("A")
+    assert v == "local"
+
+
+def test_return_default_value_when_key_is_missing(tmp_path, env_file):
+    env = load_env(env_type=EnvTypes.DEFAULT_ENV, dir_path=tmp_path)
+    v = env.get("B", "default_value")
+    assert v == "default_value"
+
+
+def test_return_bool(tmp_path, env_file):
+    env = load_env(env_type=EnvTypes.DEFAULT_ENV, dir_path=tmp_path)
+    v = env.get("BOOL")
+    assert type(v) is bool
+    assert v is True
+
+
+def test_return_int(tmp_path, env_file):
+    env = load_env(env_type=EnvTypes.DEFAULT_ENV, dir_path=tmp_path)
+    v = env.get("INT")
+    assert type(v) is int
+    assert v == 100
+
+
+def test_return_float(tmp_path, env_file):
+    env = load_env(env_type=EnvTypes.DEFAULT_ENV, dir_path=tmp_path)
+    v = env.get("FLOAT")
+    assert type(v) is float
+    assert v == 1.25
+
+
+def test_return_int_list(tmp_path, env_file):
+    env = load_env(env_type=EnvTypes.DEFAULT_ENV, dir_path=tmp_path)
+    v = env.get("INT_LIST")
+    assert type(v) is list
+    assert v == [5, 1, 2]
+
+
+def test_return_ip_list(tmp_path, env_file):
+    env = load_env(env_type=EnvTypes.DEFAULT_ENV, dir_path=tmp_path)
+    v = env.get("IP_LIST")
+    assert type(v) is list
+    assert v == ["127.0.0.1", "0.0.0.0", "localhost"]
+
+
+def test_return_string_list(tmp_path, env_file):
+    env = load_env(env_type=EnvTypes.DEFAULT_ENV, dir_path=tmp_path)
+    v = env.get("STRING_LIST")
+    assert type(v) is list
+    assert v == ["cat", "dog", "mouse", "pig"]
+
+
+def test_return_mixed_list(tmp_path, env_file):
+    env = load_env(env_type=EnvTypes.DEFAULT_ENV, dir_path=tmp_path)
+    v = env.get("MIXED_LIST")
+    assert type(v) is list
+    assert v == [1, "cat", 2.5]
